f2f15df1892066e598f2b8d3566f7dd1a75fb681
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-08-29 06:45:03 -0400

    Reading req and gen resp

--- FirstServlet/src/main/java/com/mantiso/SimpleServlet.java
+++ FirstServlet/src/main/java/com/mantiso/SimpleServlet.java
@@ -13,6 +13,22 @@ public class SimpleServlet extends HttpServlet
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        response.getWriter().write("Hello World");
+        String name = request.getParameter("name");
+        if (name != null)
+        {
+            response.setContentType("text/xml"); // html is default
+            response.getWriter().printf("<name>Hello %s</name>", name);
+        }
+        else
+            response.getWriter().write("Please enter a name value");
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+{
+    String name = request.getParameter("name"); // if not enter value from form => value is passed back in as an empty string
+    if (name != null && !name.isEmpty())
+        response.getWriter().printf("Hello %s", name);
+    else
+        response.sendRedirect("index.jsp");
+}
 }
---
+++ FirstServlet/target/simple-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Camer
+  Date: 29/8/2020
+  Time: 5:43 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="home" method="post">
+    <p>Name: <input type="text" name="name"></p>
+    <button>Enter name</button>
+</form>
+</body>
+</html>
